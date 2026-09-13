eb52eeff8630a016687a94ae4a2efa530d851b3d
abelcrvg <anonymopro@gmail.com>
2026-09-12 22:20:04 -0300

    fix: remove unused Supabase import from main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'supabase_config.dart';
 import 'theme.dart';
 import 'pages/auth_gate.dart';
